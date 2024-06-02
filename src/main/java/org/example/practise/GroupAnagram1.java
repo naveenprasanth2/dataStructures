b4220452223441d1049ad7cf981dbeeae09cc74f
naveen prasanth <naveenprasanth12@gmail.com>
2024-06-02 13:57:10 +0530

    groupAnagram

--- src/main/java/org/example/practise/GroupAnagram1.java
+++ src/main/java/org/example/practise/GroupAnagram1.java
@@ -1,10 +1,12 @@
 package org.example.practise;
 
+import org.openqa.selenium.devtools.v85.webaudio.model.GraphObjectId;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class GroupAnagram {
+public class GroupAnagram1 {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("eat", "tea", "tan", "ate", "nat", "bat");
         List<List<String>> groupedAnagrams = groupAnagrams(words);
@@ -13,16 +15,16 @@ public class GroupAnagram {
     }
 
     private static List<List<String>> groupAnagrams(List<String> words) {
-        return words.stream().collect(Collectors.groupingBy(GroupAnagram::sortedString))
+        return words.stream()
+                .collect(Collectors.groupingBy(GroupAnagram1::find))
                 .values()
                 .stream()
                 .toList();
     }
 
-
-    private static String sortedString(String text){
-        char[] array = text.toCharArray();
-        Arrays.sort(array);
-        return new String(array);
+    private static String find(String word){
+        char[] words = word.toCharArray();
+        Arrays.sort(words);
+        return new String(words);
     }
 }
